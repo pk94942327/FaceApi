f8153f3afd0b75c2d40497e7df94e252ccb2f13e
TVDOMAIN\PetSjo <peter.sjostrand@tekniskaverken.se>
2017-04-24 21:34:51 +0200

    send("URL=callFace") från acessImgData(callFace) lagt till.

--- FaceRecognitionAPI/FaceApi/FaceNewApiController.cs
+++ FaceRecognitionAPI/FaceApi/FaceNewApiController.cs
@@ -29,9 +29,10 @@ namespace FaceRecognitionAPI.FaceApi
         {
             return "value";
         }
-
+        [HttpPost]
+        [Route("api/FaceNewApi")]
         // POST: api/FaceNewApi
-        public async Task<Microsoft.ProjectOxford.Face.Contract.Face[]> Post(string URL) // Är Face[] en returtyp av två dimensionella matriser med ansikten?
+        public async Task<Microsoft.ProjectOxford.Face.Contract.Face[]> Post([FromBody] string URL) // Är Face[] en returtyp av två dimensionella matriser med ansikten?
         {
             faceServiceClient = new FaceServiceClient("3455a13fefb347f6aebff7162aa7a005");
             var requiredFaceAttributes = new FaceAttributeType[] {  // Varför finns vare sig id eller attributes medtagen som de gör i foreach (var face in faces)?
--- FaceRecognitionAPI/JS/AJAX_Anrop.js
+++ FaceRecognitionAPI/JS/AJAX_Anrop.js
@@ -34,18 +34,16 @@ getFaceDataButton.addEventListener(
 */
 
 function accessImgData(callFace) {
-
-    URL = callFace;
-    var xhr = new XMLHttpRequest(); //Se fördjupad dokumentation för detta AJAX eller snarare ett AJAJ anrop anrop, https://msdn.microsoft.com/en-us/library/ms535874(v=vs.85).aspx
-
-        // Se också grundläggande förklaring om XMLHTTPRequest() https://www.w3schools.com/xml/ajax_xmlhttprequest_create.asp
+    var xhr = new XMLHttpRequest();
+    //Se fördjupad dokumentation för detta AJAX eller snarare ett AJAJ anrop anrop, https://msdn.microsoft.com/en-us/library/ms535874(v=vs.85).aspx
+    // Se också grundläggande förklaring om XMLHTTPRequest() https://www.w3schools.com/xml/ajax_xmlhttprequest_create.asp
         if (!xhr) {
             alert('Giving up :( Cannot create an XMLHTTP instance');
             return false;
         }
-
-        //xhr.open('GET', '/api/FaceNewApi');
-        /*
+    /*
+            //xhr.open('GET', '/api/FaceNewApi');
+      
             open(method,url,async,user,psw)	Specifies the request
 
             method: the request type GET or POST
@@ -53,26 +51,27 @@ function accessImgData(callFace) {
             async: true (asynchronous) or false (synchronous)
             user: optional user name
             psw: optional password
-        */
-        xhr.open("Post", "/api/FaceNewApi");
-        xhr.send(URL); /*   Se kodraden URL = callFace;
-                            send()	Sends the request to the server. Used for GET requests
-                            send(string)	Sends the request to the server. Used for POST requests
-                            Intellisence säger att URL är en "(global variable)Element URL".
-                        */
-
+    */
+        xhr.open('Post', '/api/FaceNewApi');
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send("URL=callFace");
+    /*   Se kodraden URL = callFace;
+         send()	Sends the request to the server. Used for GET requests
+         send(string)	Sends the request to the server. Used for POST requests
+         Intellisence säger att URL är en "(global variable)Element URL".
+    */
         xhr.timeout = 2000;
 
         xhr.onload = function () {
-            var face = JSON.parse(xhr.responseText);//responseText -> Returns the response data as a string
-            //responseXML -> Returns the response data as XML data
-            //JSON.parse(...) Convert responseText to JSON format.
-
-            //xhr.onload = function () {
-            //    var face = xhr.responseText;
-            //    outputText.innerHTML = face;
-            //};
-            //console.log(face); Kollar bara vad som kommer ut ur face.
+            var face = JSON.parse(xhr.responseText);
+    /*
+            responseText -> Returns the response data as a string
+            responseXML -> Returns the response data as XML data
+            JSON.parse(...) Convert responseText to JSON format.       
+            xhr.onload = function () { var face = xhr.responseText; outputText.innerHTML = face; };
+            console.log(face); 
+            Kollar bara vad som kommer ut ur face.
+    */
             xhr.ontimeout = function (e) { outputTemp.innerHTML = "Time exceded 2 seconds. Försök igen."; }
             return face;
         }
